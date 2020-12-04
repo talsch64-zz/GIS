878e8c50ec55869033a38d6e5c761284b4d085c9
Tal Schneider <talsch64@gmail.com>
2020-12-04 11:46:43 +0200

    small changes2

--- main.cpp
+++ main.cpp
@@ -17,18 +17,26 @@
 
 
 int main(int argc, char* argv[]) {
+//    GIS gis;
+//    auto loadedIds = gis.loadMapFile("C:\\Users\\Tal\\Desktop\\university\\CS\\year3\\01 - cpp\\03 - Project\\ex1\\json-files\\ways-search.json");
+//    std::vector<EntityId> foundEntityIds = gis.getEntities("way",
+//                                                           Coordinates(Longitude(-60.550612),
+//                                                                       Latitude(-23.263132)),
+//                                                           Meters(130000));
+//    const Entity *way = gis.getEntityById(EntityId{"1"});
+//    Coordinates c = Coordinates(Longitude(-60.550612),
+//                                Latitude(-23.263132));
+//    Coordinates closest = way->getGeometry()->getClosestPoint(c);
+//    Meters distance = CoordinatesMath::calculateDistance(c, closest);
     GIS gis;
-    auto loadedIds = gis.loadMapFile("C:\\Users\\Tal\\Desktop\\university\\CS\\year3\\01 - cpp\\03 - Project\\ex1\\json-files\\ways-search.json");
-    std::vector<EntityId> foundEntityIds = gis.getEntities("way",
-                                                           Coordinates(Longitude(-60.550612),
-                                                                       Latitude(-23.263132)),
-                                                           Meters(130000));
-    const Entity *way = gis.getEntityById(EntityId{"1"});
-    Coordinates c = Coordinates(Longitude(-60.550612),
-                                Latitude(-23.263132));
-    Coordinates closest = way->getGeometry()->getClosestPoint(c);
-    Meters distance = CoordinatesMath::calculateDistance(c, closest);
-
+    gis.loadMapFile("C:\\Users\\Tal\\Desktop\\university\\CS\\year3\\01 - cpp\\03 - Project\\ex1\\json-files\\nyc.json");
+    Coordinates coord(Longitude(40.731437), Latitude(-73.996967));
+    Coordinates c1 {Longitude(40.732254),Latitude(-73.996394)};
+    Coordinates c2 {Longitude(40.733566),Latitude(-73.999446)};
+    Coordinates closest = CoordinatesMath::closestPointOnSegment(coord, c1, c2);
+    std::cout.precision(10);
+    std::cout << "closest: longitude: " << closest.longitude() << ", latitude: " << closest.latitude() << std::endl;
+    double distance = CoordinatesMath::calculateDistance(closest, {Longitude(40.73225399999999), Latitude(-73.9963939999)});
     return 0;
 }
 
