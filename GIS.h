f5cb79173c20ae5caa8fa8a564044d4650297b9f
student <talsch64@gmail.com>
2020-12-05 22:44:04 +0200

    comments added to GIS.h

--- GIS.h
+++ GIS.h
@@ -38,11 +38,15 @@ public:
 
     std::size_t saveMapFile(const std::string &filename);
 
+    /* returns vector of EntityId of all the entities in the map with the name search_name */
     std::vector<EntityId> getEntities(const std::string &search_name);
 
-    std::vector<EntityId> getEntities(const std::string &search_name, const Coordinates &, Meters radius);
+    /* returns vector of EntityId of all the entities in the map with the name search_name in the given
+     * circle which is defined by the center Coordinates and the radius  */
+    std::vector<EntityId> getEntities(const std::string &search_name, const Coordinates &center, Meters radius);
 
-    std::optional<Coordinates> getEntityClosestPoint(const EntityId &, const Coordinates &);
+    /* returns the closest point on entity's perimeter from the given coordinates*/
+    std::optional<Coordinates> getEntityClosestPoint(const EntityId &id, const Coordinates &coordinates);
 
     /* BFS-like algorithms which spreads to all direction in an even way level-by-level and searches for the closest way.
     * Once a way found, the spreading is stopped and the function returns the closest point on all
@@ -52,9 +56,10 @@ public:
 
 private:
 
-    /* Loads all the entities inside rapidjson::Document */
+    /* loads all the entities inside rapidjson::Document */
     std::vector<EntityId> loadEntities(rapidjson::Document &document);
 
+    /* returns vector with all the entities within the radius of the given coordinates */
     std::vector<const Entity *> getEntities(const Coordinates &coordinates, Meters radius);
 
     bool filterEntityByName(const Entity *entity, const std::string &search_name);
