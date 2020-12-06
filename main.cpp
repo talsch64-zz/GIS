43fe9d460bb54e3e259411ae8d49c18c264ec384
Roy Arad <royar13@gmail.com>
2020-12-06 13:57:05 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char **argv) {
     gis.loadMapFile("russia.json");
     Coordinates coord(Longitude(90.28674), Latitude(65.77863));
     Coordinates to(Longitude(90.28177), Latitude(65.89199));
-    gis.getWayClosestPoint(coord).first;
+    gis.getWayClosestPoint(coord);
     gis.saveMapFile("output.json");
     
     ::testing::InitGoogleTest(&argc, argv);
