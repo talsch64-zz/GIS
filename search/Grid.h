556bccc5b3501888cf97e9a3edf01bfaab23c6af
student <talsch64@gmail.com>
2020-12-06 03:06:17 +0200

    safe_precision added

--- search/Grid.h
+++ search/Grid.h
@@ -41,7 +41,7 @@ public:
     Grid();
 
     static constexpr double precision = 0.01;
-    static constexpr double safe_precision = 0.015;
+    static constexpr double safe_precision = 1.1 * precision;
 
     static constexpr double meterPrecision = 1;
 
