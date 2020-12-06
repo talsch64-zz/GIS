6e1c0cb3e00c79f5511c859c12d047aa6d19ad61
Roy Arad <royar13@gmail.com>
2020-12-06 13:15:58 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,17 @@
 #include <iostream>
 #include "GIS.h"
+#include "gtest/gtest.h"
 
 using std::cout;
 using std::endl;
 
 int main() {
     GIS gis;
-    gis.loadMapFile("/home/student/Desktop/advanced-ex1/russia.json");
+    gis.loadMapFile("russia.json");
     Coordinates coord(Longitude(90.28674), Latitude(65.77863));
     Coordinates to(Longitude(90.28177), Latitude(65.89199));
-
-//    cout.precision(6);
-//    for (auto &&cell: gis.grid->grid) {
-//        cout << cell.first.longitude() << ", " << cell.first.latitude() << endl;
-//    }
-//
-//
-//    cout << "-------------------------------------------------------------------------------------------" << endl;
-    Coordinates closest = gis.getWayClosestPoint(coord).first;
-//
-//    cout << closest.latitude() << ", " << to.latitude() << endl;
-//    cout << closest.longitude() << ", " << to.longitude() << endl;
-
-    return 0;
+    gis.getWayClosestPoint(coord);
+    gis.saveMapFile("output.json");
+    
+    return RUN_ALL_TESTS();
 }
