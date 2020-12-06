21c1b65acaff1a98bfebc4e9118e77560f2a3dc4
Roy Arad <royar13@gmail.com>
2020-12-06 13:56:53 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,14 @@
 using std::cout;
 using std::endl;
 
-int main() {
+int main(int argc, char **argv) {
     GIS gis;
     gis.loadMapFile("russia.json");
     Coordinates coord(Longitude(90.28674), Latitude(65.77863));
     Coordinates to(Longitude(90.28177), Latitude(65.89199));
-    gis.getWayClosestPoint(coord);
+    gis.getWayClosestPoint(coord).first;
     gis.saveMapFile("output.json");
     
+    ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
