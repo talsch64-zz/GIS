22d2cf80f24e18e799d324060fee29bc0dc8c04f
Royar <royar13@gmail.com>
2020-12-21 21:51:57 +0200

    Add highway tests

--- tests/AStarTests2.cpp
+++ tests/AStarTests2.cpp
@@ -32,7 +32,10 @@ TEST(AStar, BasicPathTest) {
 
     Routes routes = navigation.getRoutes(startCoord, endCoord);
     const Route &shortestDistance = routes.shortestDistance();
+    const Route &shortestTime = routes.shortestTime();
     assertRoute(expectedDistanceRoute, shortestDistance);
+    assertRoute(expectedDistanceRoute, shortestTime);
+    EXPECT_TRUE(routes.isValid());
 }
 
 TEST(AStar, WrongPathsAppearCloserToTarget) {
@@ -54,5 +57,43 @@ TEST(AStar, WrongPathsAppearCloserToTarget) {
 
     Routes routes = navigation.getRoutes(startCoord, endCoord);
     const Route &shortestDistance = routes.shortestDistance();
+    const Route &shortestTime = routes.shortestTime();
     assertRoute(expectedDistanceRoute, shortestDistance);
+    assertRoute(expectedDistanceRoute, shortestTime);
+    EXPECT_TRUE(routes.isValid());
+}
+
+TEST(AStar, InvalidRouteLastWayHighway) {
+    GIS gis;
+    gis.loadMapFile("astar3.json");
+    NavigationGIS navigationGis(gis);
+    Navigation navigation(navigationGis);
+    Coordinates startCoord(Longitude(20.86616914522187),
+                           Latitude(43.99225503398053));
+    Coordinates endCoord(Longitude(20.88795928805733),
+                         Latitude(43.99705953606207));
+
+    Routes routes = navigation.getRoutes(startCoord, endCoord);
+    EXPECT_FALSE(routes.isValid());
+}
+
+TEST(AStar, HighwayTooFar) {
+    GIS gis;
+    gis.loadMapFile("astar3b.json");
+    NavigationGIS navigationGis(gis);
+    Navigation navigation(navigationGis);
+    std::vector<std::pair<EntityId, Direction>> expectedDistanceRoute = {
+            std::make_pair(EntityId("way3"), Direction::B_to_A),
+            std::make_pair(EntityId("way2"), Direction::A_to_B)
+    };
+    Coordinates startCoord(Longitude(20.86616914522187),
+                           Latitude(43.99225503398053));
+    Coordinates endCoord(Longitude(20.88795928805733),
+                         Latitude(43.99705953606207));
+
+    Routes routes = navigation.getRoutes(startCoord, endCoord);
+    const Route &shortestDistance = routes.shortestDistance();
+    const Route &shortestTime = routes.shortestTime();
+    assertRoute(expectedDistanceRoute, shortestDistance);
+    assertRoute(expectedDistanceRoute, shortestTime);
 }
