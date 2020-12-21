0863b3d21ecfab41af7bd3693621c21fe3a6c028
Royar <royar13@gmail.com>
2020-12-21 18:33:01 +0200

    Add appear closer test

--- tests/AStarTests2.cpp
+++ tests/AStarTests2.cpp
@@ -6,6 +6,7 @@
 
 void assertRoute(const std::vector<std::pair<EntityId, Direction>> &expected, const Route &actual) {
     auto routeIterator = actual.getWays().begin();
+    ASSERT_EQ(actual.getWays().size(), expected.size());
     for (auto &wayPair : expected) {
         ASSERT_EQ(wayPair.first, routeIterator->first);
         ASSERT_EQ(wayPair.second, routeIterator->second);
@@ -28,6 +29,28 @@ TEST(AStar, BasicPathTest) {
                            Latitude(-30.64052973777345));
     Coordinates endCoord(Longitude(22.273619),
                          Latitude(-30.497604));
+
+    Routes routes = navigation.getRoutes(startCoord, endCoord);
+    const Route &shortestDistance = routes.shortestDistance();
+    assertRoute(expectedDistanceRoute, shortestDistance);
+}
+
+TEST(AStar, WrongPathsAppearCloserToTarget) {
+    GIS gis;
+    gis.loadMapFile("astar2.json");
+    NavigationGIS navigationGis(gis);
+    Navigation navigation(navigationGis);
+    std::vector<std::pair<EntityId, Direction>> expectedDistanceRoute = {
+            std::make_pair(EntityId("way4"), Direction::B_to_A),
+            std::make_pair(EntityId("way2"), Direction::A_to_B),
+            std::make_pair(EntityId("way3"), Direction::A_to_B),
+            std::make_pair(EntityId("way9"), Direction::A_to_B),
+            std::make_pair(EntityId("way10"), Direction::A_to_B)
+    };
+    Coordinates startCoord(Longitude(32.85226656648769),
+                           Latitude(39.89576931048837));
+    Coordinates endCoord(Longitude(32.81894402639684),
+                         Latitude(39.92570973912446));
 
     Routes routes = navigation.getRoutes(startCoord, endCoord);
     const Route &shortestDistance = routes.shortestDistance();
