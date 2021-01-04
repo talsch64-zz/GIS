c53471bf183a7437fed24c8454ebe2583926adfb
student <talsch64@gmail.com>
2021-01-04 19:27:35 +0200

    compiling updates remaining - Common/NavigationRegistration.h, Common/AbstractNavigation.h , Navigation/Navigation.cpp

--- Navigation/Navigation_315524694.cpp
+++ Navigation/Navigation_315524694.cpp
@@ -10,15 +10,15 @@ std::unique_ptr<AbstractRoutes> Navigation_315524694::getRoutes(const Coordinate
 
 std::unique_ptr<AbstractRoutes>
 Navigation_315524694::getRoutes(const Coordinates &start, const Coordinates &end, const Restrictions &restrictions) const {
-    auto startPair = navigationGIS.getWayClosestPoint(start, restrictions);
-    if (std::get<1>(startPair) == EntityId("")) {
+    auto startTuple = navigationGIS.getWayClosestPoint(start, restrictions);
+    if (std::get<AbstractGIS::ClosestPoint::WayId>(startTuple) == EntityId("")) {
         return std::make_unique<Routes>(nullptr, nullptr, false, "No ways on earth!");
     }
-    auto endPair = navigationGIS.getWayClosestPoint(end, restrictions);
-    const AbstractWay &startWay = navigationGIS.getWay(std::get<1>(startPair));
-    const Coordinates &startPoint = std::get<0>(startPair);
-    const AbstractWay &endWay = navigationGIS.getWay(std::get<1>(endPair));
-    const Coordinates &destinationPoint = std::get<0>(endPair);
+    auto endTuple = navigationGIS.getWayClosestPoint(end, restrictions);
+    const AbstractWay &startWay = navigationGIS.getWay(std::get<AbstractGIS::ClosestPoint::WayId>(startTuple));
+    const Coordinates &startPoint = std::get<AbstractGIS::ClosestPoint::Coord>(startTuple);
+    const AbstractWay &endWay = navigationGIS.getWay(std::get<AbstractGIS::ClosestPoint::WayId>(endTuple));
+    const Coordinates &destinationPoint = std::get<AbstractGIS::ClosestPoint::Coord>(endTuple);
     if (startWay.getId() == endWay.getId()) {
         return std::make_unique<Routes>(nullptr, nullptr, false, "Routes contain only one way!");
     }
