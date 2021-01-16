8fd3587d05129470d21131359d18e999ece25e7b
Royar <royar13@gmail.com>
2021-01-16 14:19:20 +0200

    Update recursion depth to 250

--- GIS/GIS_315524694.h
+++ GIS/GIS_315524694.h
@@ -34,9 +34,9 @@ class GIS_315524694 : public AbstractGIS {
 protected:
     /**
      * Max depth of BFS to search the grid for closest way before resorting to a fallback (iterating over all the ways)
-     * Around 200 km radius
+     * Around 250 km radius
      */
-    static constexpr int max_closest_way_grid_levels = 200;
+    static constexpr int max_closest_way_grid_levels = 250;
     std::unordered_map<EntityId, std::unique_ptr<Entity>> entities;
 
 protected:
