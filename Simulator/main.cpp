7d1a8c0079578fee426599a2d899074d6bfa7f72
tal <talsch64@gmail.com>
2021-01-18 19:12:39 +0200

    main fix

--- Simulator/main.cpp
+++ Simulator/main.cpp
@@ -1,14 +1,6 @@
 #include "../Common/Restrictions.h"
 #include "Simulation.h"
 #include <cstdlib>
-#include <dlfcn.h>
-#include <string>
-#include <filesystem>
-#include <iostream>
-#include <set>
-#include <cassert>
-#include <unistd.h>
-#include <getopt.h>
 #include "Registrar.h"
 
 int main(int argc, char *argv[]) {
