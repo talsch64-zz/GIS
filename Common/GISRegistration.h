2a2c834ae366fd6c72172446303fdef29533aaf7
Royar <royar13@gmail.com>
2021-01-20 20:06:34 +0200

    Run simulation from test

--- Common/GISRegistration.h
+++ Common/GISRegistration.h
@@ -1,15 +1,18 @@
-#pragma once 
+#pragma once
 
 #include "AbstractGIS.h"
-#include <memory> 
-#include <functional> 
+#include <memory>
+#include <functional>
 
 class GISRegistration {
 public:
-  GISRegistration(std::function<std::unique_ptr<AbstractGIS>()>);
+    GISRegistration(std::function<std::unique_ptr<AbstractGIS>()>);
 };
 
+#ifndef EX1_EX2
 #define REGISTER_GIS(class_name) \
 GISRegistration register_me_##class_name \
 	([]{return std::make_unique<class_name>();} );
-
+#else
+#define REGISTER_GIS(class_name) bool GIS_TESTING{true};
+#endif
--- tests/Simulator/SimulatorTest.cpp
+++ tests/Simulator/SimulatorTest.cpp
@@ -1,19 +1,67 @@
 #include "gtest/gtest.h"
+#include "../../Simulator/Simulation.h"
 #include <fstream>
 #include <cstring>
+#include <string>
 
 
 class SimulatorTest : public ::testing::Test {
+    char *mapsDirectory = strdup("/home/student/repos/advanced-topics/advanced-ex1/tests/");
+    char *requestsDirectory = strdup("/home/student/repos/advanced-topics/advanced-ex1/tests/Simulator/");
+    char *navigationSoDirectory = strdup("/home/student/repos/advanced-topics/advanced-ex1/Navigation/old-obuntu-so");
+    char *gisSoDirectory = strdup("/home/student/repos/advanced-topics/advanced-ex1/GIS/old-obuntu-so");
+
 
 public:
     SimulatorTest() {
     }
 
     void SetUp() override {
+
     }
 
-    void TearDown() override {
+    void startSimulation(int numThreads, std::string mapFileName, std::string requestsFileName) {
+        Simulation &simulation = Simulation::getInstance();
+        char *mapPath = (char *) malloc(strlen(mapsDirectory) + mapFileName.size() + 1);
+        strcpy(mapPath, mapsDirectory);
+        strcat(mapPath, mapFileName.c_str());
+        char *requestsPath = (char *) malloc(strlen(requestsDirectory) + requestsFileName.size() + 1);
+        strcpy(requestsPath, requestsDirectory);
+        strcat(requestsPath, requestsFileName.c_str());
+        char *numThreadsChars = (char *) malloc(3);
+        sprintf(numThreadsChars, "%d", numThreads);
+        char *numThreadsTitle = strdup("-num_threads");
+        char *navigationTitle = strdup("-navigation");
+        char *gisTitle = strdup("-gis");
+        char *mapFileTitle = strdup("-map_file");
+        char *navigationRequestsTitle = strdup("-navigation_requests");
+        char *argv[] = {
+                numThreadsTitle, numThreadsChars, navigationTitle, navigationSoDirectory,
+                gisTitle, gisSoDirectory, mapFileTitle, mapPath, navigationRequestsTitle, requestsPath
+        };
+        int argc = 11;
 
+        auto &registrar = simulation.getRegistrar();
+        registrar->parseCommandLineArguments(argc, argv);
+        registrar->loadSharedLibraries();
+        simulation.startSimulation();
+
+        free(numThreadsChars);
+        free(numThreadsTitle);
+        free(navigationTitle);
+        free(gisTitle);
+        free(mapFileTitle);
+        free(navigationRequestsTitle);
+        free(mapPath);
+        free(requestsPath);
+    }
+
+    void TearDown()
+    override {
+        Simulation &simulation = Simulation::getInstance();
+        auto &registrar = simulation.getRegistrar();
+        simulation.clear(); // clear all objects from the shared libraries
+        registrar->unloadSharedLibraries();
     }
 
     std::vector<std::string> getStrangeResultsLines() {
@@ -39,8 +87,14 @@ public:
         return arr;
     }
 
+    virtual ~SimulatorTest() {
+        free(mapsDirectory);
+        free(requestsDirectory);
+        free(navigationSoDirectory);
+        free(gisSoDirectory);
+    }
 };
 
-
-TEST_F(SimulatorTest, getSegmentPartsOnWayTest1) {
+TEST_F(SimulatorTest, resultsTest) {
+    startSimulation(5, "astar.json", "requests.txt");
 }
